a6ac889867cbc53074dc2a9f2eea781937316687
Alvin Aby <abyalvin4@gmail.com>
2020-05-19 14:49:06 +0700

    rename files

--- app/src/main/java/com/example/newsapp/api/ApiClient.kt
+++ app/src/main/java/com/example/newsapp/api/ApiClient.kt
@@ -1,13 +1,10 @@
 package com.example.newsapp.api
 
-import com.example.newsapp.model.NewsModel
+import com.example.newsapp.model.News
 import io.reactivex.Single
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ApiClient {
     @GET("kurio-test/mobile-engineer-android/magazine/articles")
-    fun getNews(): Single<List<NewsModel>>
+    fun getNews(): Single<List<News>>
 }
--- app/src/main/java/com/example/newsapp/view/Holder.kt
+++ app/src/main/java/com/example/newsapp/view/Holder.kt
@@ -3,27 +3,27 @@ package com.example.newsapp.view
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.newsapp.model.NewsModel
+import com.example.newsapp.model.News
 import kotlinx.android.synthetic.main.news_list.view.*
 
-class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bind(newsModel: NewsModel) {
+class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(news: News) {
         // Thumbnail
         Glide.with(itemView)
-            .load(newsModel.thumbnail)
+            .load(news.thumbnail)
             .centerCrop()
             .into(itemView.news_img)
 
         // Title
-        itemView.news_title.text = newsModel.title
+        itemView.news_title.text = news.title
 
         // Publisher
-        itemView.news_publisher.text = newsModel.publisher.name
+        itemView.news_publisher.text = news.publisher.name
 
         // Open News
         itemView.setOnClickListener {
             if (itemView.context is ViewInterface) {
-                (itemView.context as ViewInterface).openNews(newsModel.url)
+                (itemView.context as ViewInterface).openNews(news.url)
             }
         }
     }
