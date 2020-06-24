96b3afc174c87f1da3ec4ace41272c9d3d43ef63
Alvin Aby <abyalvin4@gmail.com>
2020-06-24 10:34:39 +0700

    try CompositeDisposable

--- app/src/main/java/com/example/newsapp/presenter/Presenter.kt
+++ app/src/main/java/com/example/newsapp/presenter/Presenter.kt
@@ -1,23 +1,23 @@
 package com.example.newsapp.presenter
 
-import android.util.Log
 import com.example.newsapp.api.ApiService
 import com.example.newsapp.view.ViewInterface
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 class Presenter(private val view: ViewInterface): PresenterInterface {
-    private var disposable: Disposable? = null
     private val apiGetNews = ApiService().retrofit().getNews()
+    private val compositeDisposable = CompositeDisposable()
 
     override fun getNews() {
-        disposable = apiGetNews
+        compositeDisposable.add(apiGetNews
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { result -> view.onSuccess(result) },
                 { view.onError() }
             )
+        )
     }
 }
